3521633ee8005074a0a3640fe98fd2f01b56f1d6
divyankasaxena <153445053+divyankasaxena@users.noreply.github.com>
2025-09-17 08:47:34 +0530

    Create q1.java

---
+++ level-2-array/q1.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class EmployeeBonus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double[] salary = new double[10];
+        double[] years = new double[10];
+        double[] bonus = new double[10];
+        double[] newSalary = new double[10];
+
+        double totalOldSalary = 0, totalBonus = 0, totalNewSalary = 0;
+
+     
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Enter salary of employee " + (i + 1) + ": ");
+            double s = sc.nextDouble();
+            System.out.println("Enter years of service of employee " + (i + 1) + ": ");
+            double y = sc.nextDouble();
+
+
+            if (s <= 0 || y < 0) {
+                System.out.println("Invalid input! Enter again.");
+                i--; 
+                continue;
+            }
+
+            salary[i] = s;
+            years[i] = y;
+        }
+
+       
+        for (int i = 0; i < 10; i++) {
+            if (years[i] > 5) {
+                bonus[i] = salary[i] * 0.05;
+            } else {
+                bonus[i] = salary[i] * 0.02;
+            }
+
+            newSalary[i] = salary[i] + bonus[i];
+
+            totalOldSalary += salary[i];
+            totalBonus += bonus[i];
+            totalNewSalary += newSalary[i];
+        }
+
+
+        System.out.println("\n=== Company Bonus Report ===");
+        System.out.println("Total Old Salary: " + totalOldSalary);
+        System.out.println("Total Bonus Payout: " + totalBonus);
+        System.out.println("Total New Salary: " + totalNewSalary);
+
+        sc.close();
+    }
+}
