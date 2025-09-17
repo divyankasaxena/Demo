c972e05ec32720d8aa6fd3276963528f941cc631
divyankasaxena <153445053+divyankasaxena@users.noreply.github.com>
2025-09-17 08:33:57 +0530

    Create q1.java

---
+++ level1-control-flow/q1.java
@@ -0,0 +1,14 @@
+package Level1_control_flow;
+
+import java.util.Scanner;
+
+public class ques1 {
+   public ques1() {
+   }
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int number = sc.nextInt();
+      System.out.println("Is the number " + number + " divisible by 5? " + (number % 5 == 0));
+   }
+}
