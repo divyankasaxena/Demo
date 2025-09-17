e3646217df3c56a73a836356a274e00d0f8d1b79
divyankasaxena <153445053+divyankasaxena@users.noreply.github.com>
2025-09-17 09:04:28 +0530

    Create q5.java

---
+++ level-2-java-method/q5.java
@@ -0,0 +1,29 @@
+public class UnitConverter2 {
+    public static double convertYardsToFeet(double yards) {
+        return yards * 3;
+    }
+
+    public static double convertFeetToYards(double feet) {
+        return feet * 0.333333;
+    }
+
+    public static double convertMetersToInches(double meters) {
+        return meters * 39.3701;
+    }
+
+    public static double convertInchesToMeters(double inches) {
+        return inches * 0.0254;
+    }
+
+    public static double convertInchesToCm(double inches) {
+        return inches * 2.54;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("2 yards = " + convertYardsToFeet(2) + " feet");
+        System.out.println("12 feet = " + convertFeetToYards(12) + " yards");
+        System.out.println("1 meter = " + convertMetersToInches(1) + " inches");
+        System.out.println("10 inches = " + convertInchesToMeters(10) + " meters");
+        System.out.println("5 inches = " + convertInchesToCm(5) + " cm");
+    }
+}
