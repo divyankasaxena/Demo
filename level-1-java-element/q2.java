f8848c9e92180832a11da73170b63f76ae6a1049
divyankasaxena <153445053+divyankasaxena@users.noreply.github.com>
2025-09-17 08:45:01 +0530

    Create q2.java

---
+++ level-1-java-element/q2.java
@@ -0,0 +1,14 @@
+package Level1_java_element;
+
+public class Ques2 {
+	   public Ques2() {
+	   }
+
+	   public static void main(String[] args) {
+	      int maths = 94;
+	      int physics = 95;
+	      int chemistry = 96;
+	      double average = (double)(maths + physics + chemistry) / 3.0;
+	      System.out.println("Sam’s average mark in PCM is " + average);
+	   }
+	}
