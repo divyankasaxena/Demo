4763a0c9ab076240fd1705e3cc6ea3f11c343774
divyankasaxena <153445053+divyankasaxena@users.noreply.github.com>
2025-09-17 08:45:29 +0530

    Create q3.java

---
+++ level-1-java-element/q3.java
@@ -0,0 +1,12 @@
+package Level1_java_element;
+
+public class Ques3 {
+	   public Ques3() {
+	   }
+
+	   public static void main(String[] args) {
+	      double km = 10.8;
+	      double miles = km / 1.6;
+	      System.out.println("The distance " + km + " km in miles is " + miles);
+	   }
+	}
