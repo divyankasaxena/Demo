a692d3b12e11e6ec1e5224e28d91ab5bf2887838
divyankasaxena <153445053+divyankasaxena@users.noreply.github.com>
2025-09-17 08:36:42 +0530

    Create q6.java

---
+++ level1-control-flow/q6.java
@@ -0,0 +1,21 @@
+package Level1_control_flow;
+
+import java.util.Scanner;
+
+public class ques6 {
+   public ques6() {
+   }
+
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      if (n > 0) {
+         System.out.println("positive");
+      } else if (n < 0) {
+         System.out.println("negative");
+      } else {
+         System.out.println("zero");
+      }
+
+   }
+}
